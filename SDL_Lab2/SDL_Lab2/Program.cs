b2d4bf87625c10b652371cccc70c3d5a96eaa9ca
Maxim Ermoshyn <emaxan1997@gmail.com>
2018-09-23 23:00:03 +0300

    Add n-coal figure Add rotating, scaling and transition Fix computation of u

--- SDL_Lab2/SDL_Lab2/Program.cs
+++ SDL_Lab2/SDL_Lab2/Program.cs
@@ -2,10 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Windows.Forms.VisualStyles;
 using SDL2;
 
 namespace SDL_Lab1
@@ -14,6 +10,17 @@ namespace SDL_Lab1
     {
         private const int WindowHeight = 720;
         private const int WindowWidth = 1280;
+        private const int CountOfVertex = 10;
+        private const int CountOfInnerFigure = 10;
+        private const int CircleRadius = 300;
+        private const double DeltaAngle = Math.PI/180;
+        private const int DeltaTrans = 10;
+        private const int DeltaScale = 2;
+        private const double InnerAngle = Math.PI/4;
+        private static double _scale = 1d;
+        private static double _angle = 0d;
+        private static int _transX = 0;
+        private static int _transY = 0;
         private static IntPtr _window;
         private static IntPtr _renderer;
         /// <summary>
@@ -53,6 +60,41 @@ namespace SDL_Lab1
             {
                 case SDL.SDL_EventType.SDL_QUIT:
                     return true;
+                case SDL.SDL_EventType.SDL_KEYDOWN:
+                    switch (sdlEvent.key.keysym.sym)
+                    {
+                        case SDL.SDL_Keycode.SDLK_UP:
+                            _angle += DeltaAngle;
+                            break;
+                        case SDL.SDL_Keycode.SDLK_DOWN:
+                            _angle -= DeltaAngle;
+                            break;
+                        case SDL.SDL_Keycode.SDLK_RIGHT:
+                            _scale *= DeltaScale;
+                            break;
+                        case SDL.SDL_Keycode.SDLK_LEFT:
+                            _scale /= DeltaScale;
+                            break;
+                        case SDL.SDL_Keycode.SDLK_s:
+                            _transY += DeltaTrans;
+                            break;
+                        case SDL.SDL_Keycode.SDLK_w:
+                            _transY -= DeltaTrans;
+                            break;
+                        case SDL.SDL_Keycode.SDLK_d:
+                            _transX += 10;
+                            break;
+                        case SDL.SDL_Keycode.SDLK_a:
+                            _transX -= 10;
+                            break;
+                        case SDL.SDL_Keycode.SDLK_z:
+                            _transX = 0;
+                            _transY = 0;
+                            break;
+                        case SDL.SDL_Keycode.SDLK_F4:
+                            return true;
+                    }
+                    break;
             }
 
             return false;
@@ -64,39 +106,38 @@ namespace SDL_Lab1
             SDL.SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
             SDL.SDL_RenderClear(renderer);
             DrawAxis(renderer);
-            DrawRectangle(renderer);
+            DrawFigure(renderer);
         }
 
-        private static void DrawRectangle(IntPtr renderer)
+        private static void DrawFigure(IntPtr renderer)
         {
-            var u = 0.1;
-            var countOfRectangles = 10;
-            List<SDL.SDL_Point> points = null;
-            InitStartRectanglePoints(out points);
-            var currentRectanglePoints = points;
-            currentRectanglePoints.Add(currentRectanglePoints.FirstOrDefault());
-            currentRectanglePoints = MapListOfPoints(currentRectanglePoints);
+            var currentAngle = InnerAngle/CountOfInnerFigure;
+            var u = Math.Tan(currentAngle)/(1+Math.Tan(currentAngle));
+            InitStartFigurePoints(CountOfVertex, out var points);
+            var currentFigurePoints = points;
+            currentFigurePoints.Add(currentFigurePoints.FirstOrDefault());
+            currentFigurePoints = MapListOfPoints(currentFigurePoints);
             SDL.SDL_SetRenderDrawColor(renderer, 255, 34, 45, 255);
-            SDL.SDL_RenderDrawLines(renderer, currentRectanglePoints.ToArray(), points.Count);
+            SDL.SDL_RenderDrawLines(renderer, currentFigurePoints.ToArray(), points.Count);
 
-            for (var i = 0; i < countOfRectangles; i++)
+            for (var i = 0; i < CountOfInnerFigure; i++)
             {
-                currentRectanglePoints = CalculateNewRectanglePoints(currentRectanglePoints, u);
-                currentRectanglePoints.Add(currentRectanglePoints.FirstOrDefault());
-                SDL.SDL_RenderDrawLines(renderer, currentRectanglePoints.ToArray(), currentRectanglePoints.Count);
+                currentFigurePoints = CalculateNewFigurePoints(currentFigurePoints, u);
+                currentFigurePoints.Add(currentFigurePoints.FirstOrDefault());
+                SDL.SDL_RenderDrawLines(renderer, currentFigurePoints.ToArray(), currentFigurePoints.Count);
             }
             SDL.SDL_RenderPresent(renderer);
         }
 
-        private static List<SDL.SDL_Point> CalculateNewRectanglePoints(List<SDL.SDL_Point> currentRectanglePoints, double u)
+        private static List<SDL.SDL_Point> CalculateNewFigurePoints(List<SDL.SDL_Point> currentFigurePoints, double u)
         {
             var result = new List<SDL.SDL_Point>();
-            for (var j = 0; j < currentRectanglePoints.Count - 1; j++)
+            for (var j = 0; j < currentFigurePoints.Count - 1; j++)
             {
-                var x = currentRectanglePoints[j].x;
-                var xPlus1 = currentRectanglePoints[j + 1].x;
-                var y = currentRectanglePoints[j].y;
-                var yPlus1 = currentRectanglePoints[j + 1].y;
+                var x = currentFigurePoints[j].x;
+                var xPlus1 = currentFigurePoints[j + 1].x;
+                var y = currentFigurePoints[j].y;
+                var yPlus1 = currentFigurePoints[j + 1].y;
 
                 var resultX = (1 - u) * x + u * xPlus1;
                 var resultY = (1 - u) * y + u * yPlus1;
@@ -109,15 +150,15 @@ namespace SDL_Lab1
             return result;
         }
 
-        private static void InitStartRectanglePoints(out List<SDL.SDL_Point> points)
+        private static void InitStartFigurePoints(int vertexCount, out List<SDL.SDL_Point> points)
         {
-            points = new List<SDL.SDL_Point>
+            points = new List<SDL.SDL_Point>();
+            for (var i = 0; i < vertexCount; i++)
             {
-                new SDL.SDL_Point{ x = -300, y = -300},
-                new SDL.SDL_Point{x = 300, y = -300},
-                new SDL.SDL_Point{x = 300, y = 300},
-                new SDL.SDL_Point{x = -300, y = 300}
-            };
+                var xi = (int) (CircleRadius*Math.Cos(-Math.PI/2 + 2*Math.PI*i/vertexCount));
+                var yi = (int) (CircleRadius*Math.Sin(-Math.PI/2 + 2*Math.PI*i/vertexCount));
+                points.Add(new SDL.SDL_Point { x = xi, y = yi });
+            }
         }
 
         private static void DrawAxis(IntPtr renderer)
@@ -128,20 +169,24 @@ namespace SDL_Lab1
             SDL.SDL_RenderDrawLine(renderer, width / 2, 0, width / 2, height);
         }
 
-        private static void FindCenter(ref SDL.SDL_Point centerPoint)
+        private static void FindCenter(out SDL.SDL_Point centerPoint)
         {
             SDL.SDL_GetWindowSize(_window, out var width, out var height);
-            centerPoint.x = width / 2;
-            centerPoint.y = height / 2;
-        }
+            centerPoint = new SDL.SDL_Point
+            {
+                x = width/2,
+                y = height/2
+            };
 
+        }
 
         private static void MapToPoint(int x, int y, ref SDL.SDL_Point newPoint)
         {
-            var centralPoint = new SDL.SDL_Point();
-            FindCenter(ref centralPoint);
-            newPoint.x = x + centralPoint.x;
-            newPoint.y = y + centralPoint.y;
+            FindCenter(out var centralPoint);
+            var tx = (int)(x*Math.Cos(_angle) - y*Math.Sin(_angle));
+            var ty = (int)(x*Math.Sin(_angle) + y*Math.Cos(_angle));
+            newPoint.x = centralPoint.x + (int)(tx*_scale) + _transX;
+            newPoint.y = centralPoint.y + (int)(ty*_scale) + _transY;
         }
 
         private static List<SDL.SDL_Point> MapListOfPoints(List<SDL.SDL_Point> points)
